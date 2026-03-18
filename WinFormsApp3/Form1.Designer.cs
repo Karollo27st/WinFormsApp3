4feef7f2e9b54e32e778455aa9bec3a59357d173
Karollo27st <Nowak.Karol@uczen.zselektryk.slupsk.pl>
2026-03-18 08:43:36 +0100

    Dodaj pliki projektów.

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            skibidi = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            label4 = new Label();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            label6 = new Label();
+            skibidi.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(166, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Minuty";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(386, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(52, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Sekundy";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(136, 184);
+            button1.Name = "button1";
+            button1.Size = new Size(97, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(354, 184);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Stop";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(136, 106);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(354, 106);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("SimSun", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(212, 258);
+            label3.Name = "label3";
+            label3.Size = new Size(145, 48);
+            label3.TabIndex = 6;
+            label3.Text = "00:00";
+            // 
+            // skibidi
+            // 
+            skibidi.Controls.Add(tabPage1);
+            skibidi.Controls.Add(tabPage2);
+            skibidi.Location = new Point(17, 2);
+            skibidi.Name = "skibidi";
+            skibidi.SelectedIndex = 0;
+            skibidi.Size = new Size(751, 410);
+            skibidi.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(label6);
+            tabPage1.Controls.Add(button4);
+            tabPage1.Controls.Add(button3);
+            tabPage1.Controls.Add(textBox4);
+            tabPage1.Controls.Add(textBox3);
+            tabPage1.Controls.Add(label5);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(743, 382);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Stoper";
+            tabPage1.UseVisualStyleBackColor = true;
+            tabPage1.Click += tabPage1_Click;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(label1);
+            tabPage2.Controls.Add(label3);
+            tabPage2.Controls.Add(label2);
+            tabPage2.Controls.Add(button2);
+            tabPage2.Controls.Add(textBox1);
+            tabPage2.Controls.Add(button1);
+            tabPage2.Controls.Add(textBox2);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(743, 382);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Minutnik";
+            tabPage2.UseVisualStyleBackColor = true;
+            tabPage2.Click += tabPage2_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(195, 60);
+            label4.Name = "label4";
+            label4.Size = new Size(45, 15);
+            label4.TabIndex = 0;
+            label4.Text = "Minuty";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(396, 60);
+            label5.Name = "label5";
+            label5.Size = new Size(52, 15);
+            label5.TabIndex = 1;
+            label5.Text = "Sekundy";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(164, 97);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(363, 97);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(167, 152);
+            button3.Name = "button3";
+            button3.Size = new Size(97, 34);
+            button3.TabIndex = 4;
+            button3.Text = "Start";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(363, 152);
+            button4.Name = "button4";
+            button4.Size = new Size(100, 34);
+            button4.TabIndex = 5;
+            button4.Text = "Stop";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label6.Location = new Point(246, 202);
+            label6.Name = "label6";
+            label6.Size = new Size(142, 65);
+            label6.TabIndex = 6;
+            label6.Text = "00:00";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(skibidi);
+            Name = "Form1";
+            Text = "Form1";
+            skibidi.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private TabControl skibidi;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label label6;
+        private Button button4;
+        private Button button3;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private Label label5;
+        private Label label4;
+    }
+}
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,30 @@
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
